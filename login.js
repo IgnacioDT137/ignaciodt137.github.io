bd260c27b280e63c7a957d509edb74cf921e7135
IgnacioDT137 <80978798+IgnacioDT137@users.noreply.github.com>
2022-05-08 18:30:47 -0400

    añadida la validación del login

---
+++ login.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#username').on("input", function(){
+        if($('#username').val() == ""){
+            $('#username').removeClass("ok")
+            $('#username').addClass("error")
+        } else {
+            $("#username").removeClass("error")
+            $("#username").addClass("ok")
+        }
+    });
+
+    $('#passwordinput').on("input", function(){
+        if($('#passwordinput').val() == ""){
+            $("#passwordinput").removeClass("ok")
+            $("#passwordinput").addClass("error")
+        } else {
+            $("#passwordinput").removeClass("error")
+            $("#passwordinput").addClass("ok")
+        }
+    });
+    
+    $('#form1').submit(function(){
+        if($('#username').val() == ""){
+            alert('El usuario está vacío')
+            event.preventDefault()
+        } else if($('#passwordinput').val() == ""){
+            alert('La contraseña está vacía')
+            event.preventDefault()
+        } else {
+            alert('Formulario enviado')
+        }
+    })
+})
